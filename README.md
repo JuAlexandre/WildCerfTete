7e6c44db7015d410c12ab0500f81e0e761094edd
Julien Alexandre <julien.alexandre26@gmail.com>
2018-03-15 17:26:16 +0100

    Réorganisation des fichiers Nettoyage des fichiers .css et .js

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# Wild Code School - Projet 1 : Wild Cerf-Tête
+# Wild Cerf-Tête
+> Création d'un site vitrine pour un serre-tête connecté !
 
-Création d'un site vitrine pour un serre-tête connecté !
+Wild Code School 2018 Orléans - Projet 1
+
+Création d'un site vitrine pour le lancement d'un serre-tête connecté révolutionnaire permettant de dialoguer avec les Cerfs. Ce produit est commercialisé par la société fictive WildX.
+
+![](header.png)
+
+Ce site a été imaginé et réalisé par Louise ROY et Julien ALEXANDRE.
---
+++ js/annimations.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    
+    // Animation during an action on a link in the page
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+    });
+
+
+    // Closes the drop-down menu in mobile format
+    var navMain = $(".navbar-collapse");
+    navMain.on("click", "a:not([data-toggle])", null, function () {
+        navMain.collapse('hide');
+    });
+        
+});
